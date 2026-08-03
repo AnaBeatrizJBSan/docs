ccbb68f99e20a35e95d4a296788bd6933e980a59
Alisson Nunes <dev.alynva@gmail.com>
2026-08-03 14:08:34 -0300

    fix: atualização no link de convite do Discord

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,7 +39,7 @@ export default defineConfig({
 			},
 			social: [
 				{ icon: 'external', label: 'Website', href: 'https://wiredclub.com.br/' },
-				{ icon: 'discord', label: 'Discord', href: 'https://wiredclub.com.br/discord' },
+				{ icon: 'discord', label: 'Discord', href: 'https://discord.wiredclub.com.br' },
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/wiredclub/docs' },
 			],
 			sidebar: [
